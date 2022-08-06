e514017d7c5372b26af0999a1004dbbbc217ace7
Nasta <109822733+nasta11@users.noreply.github.com>
2022-08-06 19:36:45 +0300

    дз с#2

---
+++ 002/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+int max = 0;
+ 
+max = a;
+if (b > a)
+    max = b;
+if (c > max)
+    max = c;
+
+{
+Console.WriteLine("самое большое число = " + max);
+}
+
+Console.ReadKey();
+
