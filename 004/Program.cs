fe1c2385ca793e277c961631be72667b1f35fe29
Nasta <109822733+nasta11@users.noreply.github.com>
2022-08-07 18:26:15 +0300

    дз с#4

---
+++ 004/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()); 
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (a / 2 == 0 );
+
+
+
+{
+Console.WriteLine("Введенное число является четным");
+}
+
+
+if (a / 2 == 1 );
+
+{
+Console.WriteLine("Введенное число является нечетным");
+}
+
+
+Console.Read();
